4f38ce3ee14f24e279984b64bff99f0aa41a010e
nealb <sb6856@nyu.edu>
2018-09-21 19:21:36 -0400

    Addition to Encrypt Junit

--- project1/src/test/java/EncryptJunit.java
+++ project1/src/test/java/EncryptJunit.java
@@ -8,17 +8,32 @@ public class EncryptJunit {
 
 	@Test
 	public void encryptmessage() {
-		
+
 		System.out.println("Enter the message");
 		Scanner sc = new Scanner(System.in);
 		String message = sc.nextLine();
 		sc.close();
 		if (message.length() > 500) {
-			message = message.substring(0, 499);
+			message = message.substring(0, 500);
 		}
 		Encrypt encrypt = new Encrypt(message);
+		System.out.println(message.length());
 		String ciphertext = encrypt.getEncryptedMessage();
 		System.out.println("The cipher text is :"+ciphertext);
+		String[] split = ciphertext.split(",");
+		System.out.println(split.length);
+		HashMap<String, Integer> frequency_maps = new HashMap<String, Integer>();
+		for (String letter: split) {
+			if (frequency_maps.containsKey(letter)) {
+				int count = frequency_maps.get(letter) + 1;
+				frequency_maps.put(letter, count);
+			} else {
+				frequency_maps.put(letter, 0);
+			}
+		}
+		for (String val: frequency_maps.keySet()) {
+			System.out.println(val + "--->" + frequency_maps.get(val));
+		}
 	
 	}
 	
