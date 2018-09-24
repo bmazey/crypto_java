8a6230b18ba8d81fe17f4ca82ca2f02dfad054bd
Brandon <b.mazey@nyu.edu>
2018-09-24 16:58:57 -0400

    omg this is making sense ...

--- project1/src/main/java/org/nyu/decrypt/Backtracker.java
+++ project1/src/main/java/org/nyu/decrypt/Backtracker.java
@@ -1,11 +1,32 @@
 package org.nyu.decrypt;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.nyu.dto.Dictionary;
+import org.nyu.dto.Frequency;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.File;
+import java.io.IOException;
+
 public class Backtracker {
 
-    //TODO - implement backtracking!
+    public Dictionary dictionary;
+    public Frequency frequency;
 
-    public void backtrack() {
+    //TODO - implement backtracking!
+    public Backtracker() throws IOException {
+        File frequencyFile = new ClassPathResource("frequency.json").getFile();
+        File dictionaryFile = new ClassPathResource("dictionary.json").getFile();
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        this.dictionary = objectMapper.readValue(dictionaryFile, Dictionary.class);
+        this.frequency = objectMapper.readValue(frequencyFile, Frequency.class);
 
     }
 
+
+    public void backtrack(String ciphertext) {
+
+
+    }
 }
