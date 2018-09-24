b31e0fb8b4cbb71378b851cd7794c8b538b311b3
Brandon <b.mazey@nyu.edu>
2018-09-24 14:32:47 -0400

    Dictionary looking good ....

--- project1/src/main/java/org/nyu/dto/Dictionary.java
+++ project1/src/main/java/org/nyu/dto/Dictionary.java
@@ -1,4 +1,20 @@
 package org.nyu.dto;
 
 public class Dictionary {
+
+    /**
+     * This class represents a Dictionary POJO
+     * b.mazey@nyu.edu
+     */
+
+    private String[] words;
+
+    public String[] getWords() {
+        return words;
+    }
+
+    public void setWords(String[] words) {
+        this.words = words;
+    }
+
 }
