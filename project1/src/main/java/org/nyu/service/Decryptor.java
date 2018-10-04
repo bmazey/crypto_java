bfa0fbfde32d2013669eb2235316a454fb5b4bb1
Brandon <b.mazey@nyu.edu>
2018-10-04 15:04:35 -0400

    all set! still needs cleanup

--- project1/src/main/java/org/nyu/service/Decryptor.java
+++ project1/src/main/java/org/nyu/service/Decryptor.java
@@ -1,4 +1,30 @@
 package org.nyu.service;
 
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+
+@Service
 public class Decryptor {
+
+    public String decrypt(HashMap<String, ArrayList<Integer>> map, int[] ciphertext) {
+        StringBuilder plaintext = new StringBuilder();
+
+        for(int i = 0; i < ciphertext.length; i++) {
+
+            for(String key : map.keySet()) {
+                ArrayList values = map.get(key);
+                if (values.contains(ciphertext[i])) {
+
+                    //This is bad - don't concat strings this way!
+                    if (key.equals("space")) plaintext.append(" ");
+                    else plaintext.append(key);
+                }
+            }
+        }
+
+        return plaintext.toString();
+    }
 }
---
+++ project1/src/test/java/PartOneTest.java
@@ -0,0 +1,52 @@
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.nyu.ProjectOneApplication;
+import org.nyu.dto.Candidates;
+import org.nyu.service.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Random;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes=ProjectOneApplication.class)
+public class PartOneTest {
+
+    @Autowired
+    CandidateGenerator candidateGenerator;
+
+    @Autowired
+    FrequencyGenerator frequencyGenerator;
+
+    @Autowired
+    KeyGenerator keyGenerator;
+
+    @Autowired
+    Encryptor encryptor;
+
+    @Autowired
+    Decryptor decryptor;
+
+    private Random r;
+
+
+    @Test
+    public void partOne() throws IOException {
+
+        r = new Random();
+        Candidates candidates = candidateGenerator.generateCandidates();
+
+        // now we'll draft a random candidate ...
+        String candidate = candidates.getCandidates()[r.nextInt(candidates.getCandidates().length)];
+
+        int[] ciphertext = encryptor.encrypt(keyGenerator.generateKey(), candidate);
+
+        System.out.println("test: " + Arrays.toString(ciphertext));
+
+
+    }
+}
